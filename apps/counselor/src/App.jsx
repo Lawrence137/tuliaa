3bb72b4dcb1179ed9218d037371a50817d80bf0c
Lawrence Kimani <lawrencekimani18@gmail.com>
2025-12-14 18:01:07 +0300

    creates counselor dashboard

--- apps/counselor/src/App.jsx
+++ apps/counselor/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Auth/Login';
+import CounselorDashboard from './pages/Dashboard/CounselorDashboard';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         {/* Placeholder for other routes */}
-        <Route path="/dashboard" element={<div>Counselor Dashboard</div>} />
+        <Route path="/dashboard" element={<CounselorDashboard />} />
         <Route path="/forgot-password" element={<div>Forgot Password Page</div>} />
         <Route path="/signup" element={<div>Sign Up Page</div>} />
       </Routes>
