c7722961be003ca4d90c30908d08ca0bf1c7fa16
Lawrence Kimani <lawrencekimani18@gmail.com>
2025-12-14 17:43:18 +0300

    creates login page

--- apps/counselor/src/App.jsx
+++ apps/counselor/src/App.jsx
@@ -1,9 +1,18 @@
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Auth/Login';
 
 function App() {
   return (
-    <h1 className="bg-red-500" >Counselor App</h1>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {/* Placeholder for other routes */}
+        <Route path="/dashboard" element={<div>Counselor Dashboard</div>} />
+        <Route path="/forgot-password" element={<div>Forgot Password Page</div>} />
+        <Route path="/signup" element={<div>Sign Up Page</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
